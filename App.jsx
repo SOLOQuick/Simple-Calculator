127bc56ee065e261e74db634434cc629ab6cc615
SOLOQuick <sakshisingh4102005@gmail.com>
2025-07-11 21:39:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+
+import Calculator from './components/Calculator'
+
+function App() {
+  return (
+    <>
+    <Calculator/>
+
+    </>
+  )
+}
+
+export default App
